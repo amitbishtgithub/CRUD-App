b11db41a42490b444320b9ae661ed5c0a6be8afc
Amit_Bisht <abisht021@gmail.com>
2024-09-20 22:12:30 +0530

    created component for table using tanstack-table

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useMemo } from "react";
+import mdata from "../MOCK_DATA.json";
+import { DateTime } from "luxon";
+import ReactTable from "./ReactTable";
 
 function Create() {
+
+  const data = useMemo(() => mdata, []);
+
+  /** @type import('@tanstack/react-table').ColumnDef<any>*/
+  const columns = [
+    {
+      header: "ID",
+      accessorKey: "id",
+    },
+
+    // to show multiple cols under one header
+    // {
+    //   header: 'Name',
+    //   columns: [
+    //     {
+    //       header: 'First',
+    //       accessorKey: 'first_name'
+    //     },
+    //     {
+    //       header: 'Last',
+    //       accessorKey: 'last_name'
+    //     }
+    //   ]
+    // },
+
+    // for showing first and last name together!
+    // {
+    //   header: "Name",
+    //   accessorFn: row => `${row.first_name} ${row.last_name}`
+    // },
+
+    {
+      header: "First Name",
+      accessorKey: "first_name",
+    },
+    {
+      header: "Last Name",
+      accessorKey: "last_name",
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+    },
+    {
+      header: "Gender",
+      accessorKey: "gender",
+    },
+    {
+      header: "Date Of Birth",
+      accessorKey: "dob",
+      cell: (info) =>
+        DateTime.fromISO(info.getValue()).toLocaleString(DateTime.DATE_MED),
+    },
+  ];
+
   return (
     <>
-      <div className="row">
-        <div className="col-md-4">
-          <form action="">
-            <div className="form-group">
-              <label htmlFor="Enter Name">First Name</label>
-              <input type="text" placeholder="Enter Name"/>
-            </div>
-          </form>
-        </div>
-      </div>
+      <ReactTable data = {data} columns = {columns}/>
     </>
   );
 }
