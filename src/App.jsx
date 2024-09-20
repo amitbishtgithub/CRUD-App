4d13eb55c3b420933f19330130121d0df4b2e31a
Amit_Bisht <abisht021@gmail.com>
2024-09-20 12:59:13 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Create from "./components/Create";
+
+function App() {
+  return (
+    <div className="container">
+      <Create />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Create.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Create() {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-4">
+          <form action="">
+            <div className="form-group">
+              <label htmlFor="Enter Name">First Name</label>
+              <input type="text" placeholder="Enter Name"/>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Create;
