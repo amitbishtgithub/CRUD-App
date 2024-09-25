3d6cdb308134dc406e803f0d97d515b6f9fadc31
Amit_Bisht <abisht021@gmail.com>
2024-09-25 23:15:21 +0530

    Changes for Add Details modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,7 +1,118 @@
-import React from 'react'
+import React from "react";
+import { Form, useForm } from "react-hook-form";
+import "../../src/index.css";
+import {
+  Col,
+  FormGroup,
+  Input,
+  Label,
+  ModalFooter,
+  ModalHeader,
+  Row,
+} from "reactstrap";
 
 export default function Add() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  async function onSubmit(data) {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("Submitting the form", data);
+  }
+
   return (
-    <div>Add</div>
-  )
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* <div> */}
+      <ModalHeader tag="h3"><strong>Add Details</strong></ModalHeader>
+      <FormGroup className="m-4">
+        <Row>
+          <Col sm={12} md={6}>
+            <Label>First Name</Label>
+            <Input
+              className={errors.first_Name ? "input-error" : ""}
+              {...register("first_Name", {
+                required: true,
+                minLength: { value: 3, message: "minimum 3 characters" },
+              })}
+            />
+            {errors.first_Name && (
+              <p className="error-msg">{errors.first_Name.message}</p>
+            )}
+          </Col>
+          <Col sm={12} md={6}>
+            <Label>Last Name</Label>
+            <Input
+              className={errors.last_name ? "input-error" : ""}
+              {...register("last_name", {
+                // required: true,
+                // minLength: { value: 3, message: "minimum 3 characters" },
+              })}
+            />
+            {errors.last_name && (
+              <p className="error-msg">{errors.last_name.message}</p>
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12} md={6}>
+            <Label>Email</Label>
+            <Input
+              className={errors.email ? "input-error" : ""}
+              {...register("email", {
+                required: true,
+              })}
+            />
+            {errors.email && (
+              <p className="error-msg">{errors.email.message}</p>
+            )}
+          </Col>
+          <Col sm={12} md={6}>
+            <Label>Gender</Label>
+            <Input
+              className={errors.gender ? "input-error" : ""}
+              {...register("gender", {
+                // required: true,
+                // minLength: { value: 3, message: "minimum 3 characters" },
+              })}
+            />
+            {errors.gender && (
+              <p className="error-msg">{errors.gender.message}</p>
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12} md={6}>
+            <Label>Date of Birth</Label>
+            <Input
+              type="date"
+              className={errors.dob ? "input-error" : ""}
+              {...register("dob", {
+                required: true,
+                // minLength: { value: 3, message: "minimum 3 characters" },
+              })}
+            />
+            {errors.dob && <p className="error-msg">{errors.dob.message}</p>}
+          </Col>
+        </Row>
+        <ModalFooter
+          style={{
+            width: "70%",
+            margin: "auto",
+          }}
+        >
+          <Input
+            type="submit"
+            disabled={isSubmitting}
+            value={isSubmitting ? "Submitting" : "Submit"}
+            className="primary bg-primary"
+          />
+        </ModalFooter>
+      </FormGroup>
+      {/* </div> */}
+    </form>
+  );
 }
--- src/components/TableWithPagination.jsx
+++ src/components/TableWithPagination.jsx
@@ -120,7 +120,7 @@ export default function TableWithPaginate() {
       <ReactTable data={apiData} columns={columns} />
 
       {/* Add Modal */}
-      <UncontrolledModal target="add" size="lg">
+      <UncontrolledModal target="add" size="md">
         <Add />
       </UncontrolledModal>
 
